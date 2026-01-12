948de73ad86e358572cd55d5f7a90ba1da441324
javokhir-nw <javokhirganiboyev2004@gmail.com>
2026-01-12 15:09:02 +0500

    fixed

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
-import { setupApiInterceptor } from './utils/apiInterceptor'
+import { setupApiInterceptor } from './utils/ApiInterceptor.js'
 import './style.css'
 
 const app = createApp(App)
